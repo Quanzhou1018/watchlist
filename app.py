eb1561527a719e3dca61c1f31f4811a439bb5879
Quanzhou Xiang <xqz20001018@163.com>
2022-04-17 17:02:43 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -6,26 +6,14 @@ from flask import escape,Flask,url_for,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# 在扩展类实例化前加载配置
 db=SQLAlchemy(app)
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+name = 'Quanzhou Xiang'
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-# 在扩展类实例化前加载配置
-db = SQLAlchemy(app)
 
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)   #主键
@@ -36,31 +24,19 @@ class Movie(db.Model):
     title=db.Column(db.String(60))
     year=db.Column(db.String(4))
 
-@app.cli.command()  # 注册为命令
-def forge():
-    """Generate fake data."""
-    db.create_all()
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
-    # 全局的两个变量移动到这个函数内
-    name = 'Quanzhou Xiang'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', name=name, movies=movies)
 
-    user = User(name=name)
-    db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
 
-    db.session.commit()
-    click.echo('Done.')
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html', user=user), 404  # 返回模板和状态码
+
+
